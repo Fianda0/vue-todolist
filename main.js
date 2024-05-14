1783aa8527c44d9c1181af964aae566d29517bc8
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-14 15:56:30 +0200

    Milestone 1

--- main.js
+++ main.js
@@ -13,7 +13,28 @@ createApp({
         'giocare',
         'lavarsi',
         'dormire'
-      ]
+      ],
+      addToDo: ''
+    }
+  }, methods: {
+
+    //Creo controllo sull' click
+    aggiungi() {
+      //Se la stringa è vuota ERRORE
+      if (this.addToDo == '') {
+        alert('inserire qualcosa')
+      }
+      // Se la stringa non è contenuta nella lista aggiungerla
+      else if (!this.lista.includes(this.addToDo)) {
+        this.lista.push(this.addToDo)
+        this.addToDo = ''
+      }
+      //Se la stringa è contenuta ERRORE
+      else {
+        alert('la lista contine già quel elemento')
+      }
+
+
     }
   }
 }).mount('#app')
